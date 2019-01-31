7f9150748e6334913906b8620338a284d1947504
Admin <Admin@STUDENT17>
2019-01-31 16:21:38 +0000

    .

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -17,11 +17,8 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@Column(length=100)
 	private String firstName;
-	@Column(length=50)
 	private String lastName;
-	@Column(length=10)
 	private Long accountNumber;
 
 	public Account() {
